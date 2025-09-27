2d1cdcd7287eb148485ecbeebffae7adfa81dc3a
Edward Mironov <edwardmir0515@gmail.com>
2025-09-27 10:05:58 +0500

    refactor

--- guese_the_number.py
+++ guese_the_number.py
@@ -5,8 +5,8 @@ class Comunication:
     def __init__(self,
                  text1="Welcome! This is a game where you need to guess the number.",
                  text2="Number is between 0 and 100. You have 10 tries. Type 'q' to quit.\n"):
-        self.hi_words = text1
-        self.explanaition = text2
+        self.hi_words: str = text1
+        self.explanaition: str = text2
 
     def say_hi(self):
         print(self.hi_words)
@@ -14,21 +14,18 @@ class Comunication:
 
 
 class Computer:
-    def __init__(self):
-        pass
-
     def set_computer_number(self):
         return random.randint(0, 100)
 
 
 class Guesser:
     def __init__(self):
-        self.history = []
-        self.TRIES = 10
+        self.history: list = []
+        self.TRIES: int = 10
 
     def guess_number(self, computer_number):
         while self.TRIES > 0:
-            user_answer_str = input("Your number? ").strip()
+            user_answer_str: str = input("Your number? ").strip()
 
             if not user_answer_str:
                 print("Empty, please try again!")
@@ -38,7 +35,7 @@ class Guesser:
                 print("Only numbers are allowed! Try again.")
                 continue
 
-            user_answer = int(user_answer_str)
+            user_answer: int = int(user_answer_str)
 
             if user_answer > computer_number:
                 self.TRIES -= 1
@@ -50,7 +47,7 @@ class Guesser:
                 print(f"Too low! You have {self.TRIES} tries left.")
             else:
                 print("🎉 Congratulations, you guessed it!")
-                user_input = input(
+                user_input: str = input(
                     "Do you want to see a history? y(yes)/n(no)")
                 if not user_input:
                     print("Wrong answer!")
@@ -66,17 +63,17 @@ class Guesser:
 
 
 class GuessGame:
-    def __init__(self):
-        self.user_message = Comunication()
-        self.computer = Computer()
-        self.gueser = Guesser()
+    def __init__(self, user_message: Comunication, computer: Computer, gueser: Guesser):
+        self.user_message = user_message
+        self.computer = computer
+        self.gueser = gueser
 
     def run(self):
         self.user_message.say_hi()
         while True:
-            self.number = self.computer.set_computer_number()
+            self.number: int = self.computer.set_computer_number()
             self.gueser.guess_number(self.number)
-            checker = input("Do you want to play again? y or n: ")
+            checker: str = input("Do you want to play again? y or n: ")
             if checker == "y":
                 continue
             elif checker == "n":
@@ -88,7 +85,10 @@ class GuessGame:
 
 
 def main():
-    app = GuessGame()
+    user_message = Comunication()
+    computer = Computer()
+    gueser = Guesser()
+    app = GuessGame(user_message, computer, gueser)
     app.run()
 
 
